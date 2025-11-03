ac4cc56c751e5db3b9616165d2ec9e093bfbc3ba
Victor <00143320@uca.edu.sv>
2025-11-02 23:18:48 -0600

    feature: add Product entity, lacks rel :construction:

---
+++ backend/InventoryManagementBackend/src/main/java/org/peraza/inventorymanagementbackend/Models/Entity/Product.java
@@ -0,0 +1,30 @@
+package org.peraza.inventorymanagementbackend.Models.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false, name = "product_id")
+    private UUID productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column
+    private String description;
+
+    @Column
+    private double price;
+
+    @Column
+    private int quantity;
+
+}
